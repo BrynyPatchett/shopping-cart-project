d74d05f0d8a9cb35bdb1e2639c084d502718ba2e
Bryny <28590441+BrynyPatchett@users.noreply.github.com>
2024-02-01 12:06:05 +1300

    added product card test

---
+++ src/components/products/product_card/productcard.test.jsx
@@ -0,0 +1,26 @@
+
+import '@testing-library/jest-dom';
+import { describe } from 'vitest'
+import ProductCard from './productcard'
+import { render,screen } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+
+
+describe("Product Card render",() => {
+    it("Product card renders with no props", ()=>{
+        render(<ProductCard/>,{wrapper:BrowserRouter})
+        expect(screen.findByText("$"));
+    })
+    it("Product card renders with all props", ()=>{
+        const productInfo ={
+            productId:1, title:"test product", price:25, imagesrc:"testimage"
+        }
+
+        render(<ProductCard {...productInfo} />,{wrapper:BrowserRouter})
+        expect(screen.getByRole('link')).toHaveAttribute('href', `/product/${productInfo.productId}`);
+        expect(screen.getByRole('img')).toHaveAttribute('src', `${productInfo.imagesrc}`);
+        expect(screen.findByText(productInfo.title));
+        expect(screen.findByText(productInfo.price));
+    })
+})
+
